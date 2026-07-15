47e0a3f9ebd0e5a47acfc9afb5712db44a7fdab0
Kunj Savani <kunjsavani9@gmail.com>
2026-07-15 09:18:13 +0530

    Update Code.ino

--- firmware/Code.ino
+++ firmware/Code.ino
@@ -1,7 +1,7 @@
 // ─── Fill these before uploading ──────────────────────────────────────────────
-#define BLYNK_TEMPLATE_ID   "TMPL3WQxjP_4v"
-#define BLYNK_TEMPLATE_NAME "Medicine Box"
-#define BLYNK_AUTH_TOKEN    "vD0ncWHf2FZPts3tdaRiMIXqL36o_ivP"
+#define BLYNK_TEMPLATE_ID   "<blynk_template_id"
+#define BLYNK_TEMPLATE_NAME "<blynk_template_name"
+#define BLYNK_AUTH_TOKEN    "<blynk_auth_token"
 #define BLYNK_PRINT Serial
 
 const char* WIFI_SSID     = "<wifi_name>";
